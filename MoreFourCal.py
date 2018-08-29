3bb83ccd517dd63efce2ed7126cbfe0fe3a15e12
youngpil <youngpilyun@tmon.co.kr>
2018-08-29 16:08:56 +0900

    module and example

--- MoreFourCal.py
+++ MoreFourCal.py
@@ -2,10 +2,20 @@
 from FourCal import FourCal
 
 class MoreFourCal(FourCal):
-    pass
+    def pow(self):
+        result = self.first ** self.second
+        return result
+    def div(self):
+        if self.second == 0:
+            return 0
+        else:
+            return self.first / self.second
 
 a = MoreFourCal(4, 2)
 print(a.sum())
 print(a.mul())
 print(a.sub())
 print(a.div())
+print(a.pow())
+b = MoreFourCal(4, 0)
+print(b.div())
